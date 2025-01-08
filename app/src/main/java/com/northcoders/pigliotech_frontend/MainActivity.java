f694e4e26ff0bd926f435ab32daaa85af01e806f
Tawanda Chabva <tchabva@gmail.com>
2025-01-08 22:26:49 +0000

    feat: implement firebase signup

--- app/src/main/java/com/northcoders/pigliotech_frontend/MainActivity.java
+++ app/src/main/java/com/northcoders/pigliotech_frontend/MainActivity.java
@@ -20,5 +20,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout_fragment, new SignUpFragment())
+                .commit();
     }
 }
---
+++ app/src/main/java/com/northcoders/pigliotech_frontend/SignUpFragment.java
@@ -0,0 +1,142 @@
+package com.northcoders.pigliotech_frontend;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.northcoders.pigliotech_frontend.databinding.FragmentSignUpBinding;
+
+
+public class SignUpFragment extends Fragment {
+
+    private EditText emailTextView, passwordTextView;
+    private Button Btn;
+    private ProgressBar progressbar;
+    private FirebaseAuth mAuth;
+    private FragmentSignUpBinding binding;
+
+    public SignUpFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // taking FirebaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+
+        // Inflate the layout for this fragment
+        binding = FragmentSignUpBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        emailTextView = binding.email;
+        passwordTextView = binding.passwd;
+        Btn = binding.btnregister;
+        progressbar = binding.progressbar;
+
+        Btn.setOnClickListener(view1 -> {
+            registerNewUser();
+        });
+
+    }
+
+    private void registerNewUser()
+    {
+
+        // show the visibility of progress bar to show loading
+        progressbar.setVisibility(View.VISIBLE);
+
+        // Take the value of two edit texts in Strings
+        String email, password;
+        email = emailTextView.getText().toString();
+        Log.i("EMAIL", email);
+        password = passwordTextView.getText().toString();
+        Log.i("PASSWORD", password);
+        // Validations for input email and password
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getContext(),
+                            "Please enter email!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getContext(),
+                            "Please enter password!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        // create new user or register new user
+        mAuth
+                .createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getContext(),
+                                            "Registration successful!",
+                                            Toast.LENGTH_LONG)
+                                    .show();
+
+                            // hide the progress bar
+                            progressbar.setVisibility(View.GONE);
+
+                            // if the user created intent to login activity
+                            getActivity().getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.frame_layout_fragment, new RegisteredUserFragment())
+                                    .commit();
+                        }
+                        else {
+
+                            // Registration failed
+                            Toast.makeText(
+                                            getContext(),
+                                            "Registration failed!!"
+                                                    + " Please try again later",
+                                            Toast.LENGTH_LONG)
+                                    .show();
+
+                            // hide the progress bar
+                            progressbar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+    }
+
+
+}
