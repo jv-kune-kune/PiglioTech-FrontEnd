3d6cebac04f16314217e92570ec141bf719575ae
Tawanda Chabva <tchabva@gmail.com>
2025-01-09 15:19:32 +0000

    feat: created ViewModel

---
+++ app/src/main/java/com/northcoders/pigliotech_frontend/ui/mainactivity/MainActivityViewModel.java
@@ -0,0 +1,13 @@
+package com.northcoders.pigliotech_frontend.ui.mainactivity;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class MainActivityViewModel extends AndroidViewModel {
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
