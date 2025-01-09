2b47ef3fd0b2c2ad13b8d067e594352c630d82fc
Tawanda Chabva <tchabva@gmail.com>
2025-01-09 23:23:47 +0000

    feat: implement MainActivtyViewModel
    Implemented a LiveData event observer MainActivty using MainActivityEvents as a state and a function in the ViewModel to determine the state

--- app/src/main/java/com/northcoders/pigliotech_frontend/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/northcoders/pigliotech_frontend/ui/mainactivity/MainActivity.java
@@ -9,25 +9,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.navigation.NavigationBarView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.northcoders.pigliotech_frontend.ui.fragments.addbook.AddBookFragment;
 import com.northcoders.pigliotech_frontend.ui.fragments.home.HomeFragment;
 import com.northcoders.pigliotech_frontend.ui.fragments.landingpage.LandingPageFragment;
 import com.northcoders.pigliotech_frontend.R;
 import com.northcoders.pigliotech_frontend.ui.fragments.profile.ProfileFragment;
-import com.northcoders.pigliotech_frontend.ui.fragments.signup.SignUpFragment;
 
 public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
-    private FirebaseAuth mAuth;
-    private NavigationBarView bottomNav;
-    private ProfileFragment profileFragment = new ProfileFragment(); // TODO Rename to ProfileFragment
-    private HomeFragment homeFragment = new HomeFragment();
-    private AddBookFragment addBookFragment = new AddBookFragment();
-
+    private MainActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,63 +30,58 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
             return insets;
         });
 
-        mAuth = FirebaseAuth.getInstance();
-        bottomNav = findViewById(R.id.bottom_nav_bar);
+        // Instantiate the ViewModel
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
+        // Initialising the BottomNavigationBar and assigning the setOnItemSelectedListener
+        NavigationBarView bottomNav = findViewById(R.id.bottom_nav_bar);
         bottomNav.setOnItemSelectedListener(this);
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.frame_layout_fragment, new SignUpFragment())
-                .commit();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        ProfileFragment profileFragment = new ProfileFragment();
-        LandingPageFragment landingPageFragment = new LandingPageFragment();
-
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.frame_layout_fragment, profileFragment)
-                    .commit();
-
-            NavigationBarView bottomNavBar = findViewById(R.id.bottom_nav_bar);
-
-
-        } else {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.frame_layout_fragment, landingPageFragment)
-                    .commit();
-        }
+        // Observing the MainActivityEvents LiveData from the ViewModel.
+        viewModel.getEvents().observe(this, event -> {
+            if (event != null){ // Code will only run if event is not null
+                switch (event){
+                    case NAVIGATE_TO_LANDING_PAGE:
+                        Fragment landingPage = new LandingPageFragment();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.frame_layout_fragment, landingPage)
+                                .commit();
+                        break;
+                    case NAVIGATE_TO_HOME_PAGE:
+                        Fragment homeFragment = new HomeFragment();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.frame_layout_fragment, homeFragment)
+                                .commit();
+                        break;
+                }
+                viewModel.eventSeen(); // Sets the event to null
+            }
+        });
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         if(item.getItemId() == R.id.home) {
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.frame_layout_fragment, homeFragment)
+                    .replace(R.id.frame_layout_fragment, new HomeFragment())
                     .commit();
             return true;
         }
         if(item.getItemId() == R.id.profile) {
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.frame_layout_fragment, profileFragment)
+                    .replace(R.id.frame_layout_fragment, new ProfileFragment())
                     .commit();
             return true;
         }
         if(item.getItemId() == R.id.addBook) {
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.frame_layout_fragment, addBookFragment)
+                    .replace(R.id.frame_layout_fragment, new AddBookFragment())
                     .commit();
             return true;
         }
