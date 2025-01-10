740680a5c55365dc84aa3bd1c74cbc6751ff2f60
Tawanda Chabva <tchabva@gmail.com>
2025-01-10 14:01:37 +0000

    feat: add attributes, constructors, getters and setters for User class

--- app/src/main/java/com/northcoders/pigliotech_frontend/model/User.java
+++ app/src/main/java/com/northcoders/pigliotech_frontend/model/User.java
@@ -1,5 +1,81 @@
 package com.northcoders.pigliotech_frontend.model;
 
-//TODO
-public class User {
+import androidx.databinding.BaseObservable;
+
+import java.util.List;
+
+//TODO : possibly might need to extend parcelable
+public class User extends BaseObservable {
+    private String uid;
+    private String name;
+    private String email;
+    private String region;
+    private String thumbnail;
+    private List<Book> books;
+
+    // No args constructor
+    public User() {}
+
+    // All args
+    public User(String uid, String name, String email, String region, String thumbnail, List<Book> books) {
+        this.uid = uid;
+        this.name = name;
+        this.email = email;
+        this.region = region;
+        this.thumbnail = thumbnail;
+        this.books = books;
+    }
+
+    // New User constructor
+    public User(String uid, String name, String email, String region, String thumbnail) {
+        this.uid = uid;
+        this.name = name;
+        this.email = email;
+        this.region = region;
+        this.thumbnail = thumbnail;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
 }
