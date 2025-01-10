7c33f714d37f09b0908fb438cf97a341dcd2bc85
Tawanda Chabva <tchabva@gmail.com>
2025-01-10 11:12:46 +0000

    feat: create LoginFragment Events Enum, State Class and ViewModel

---
+++ app/src/main/java/com/northcoders/pigliotech_frontend/ui/fragments/login/LoginEvents.java
@@ -0,0 +1,6 @@
+package com.northcoders.pigliotech_frontend.ui.fragments.login;
+
+public enum LoginEvents {
+    LOGIN_SUCCESSFUL,
+    LOGIN_FAILED
+}
---
+++ app/src/main/java/com/northcoders/pigliotech_frontend/ui/fragments/login/LoginViewModel.java
@@ -0,0 +1,6 @@
+package com.northcoders.pigliotech_frontend.ui.fragments.login;
+
+import androidx.lifecycle.ViewModel;
+
+public class LoginViewModel extends ViewModel {
+}
