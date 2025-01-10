0c1a3ceb51e63a115f8d3d8ed9e168fae282b6c8
Tawanda Chabva <tchabva@gmail.com>
2025-01-10 11:51:22 +0000

    feat: implement LoginFragment ViewModel

--- app/src/main/java/com/northcoders/pigliotech_frontend/ui/fragments/login/LoginFragment.java
+++ app/src/main/java/com/northcoders/pigliotech_frontend/ui/fragments/login/LoginFragment.java
@@ -1,10 +1,13 @@
 package com.northcoders.pigliotech_frontend.ui.fragments.login;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -22,7 +25,9 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.northcoders.pigliotech_frontend.R;
 import com.northcoders.pigliotech_frontend.databinding.FragmentLoginBinding;
+import com.northcoders.pigliotech_frontend.ui.fragments.home.HomeFragment;
 import com.northcoders.pigliotech_frontend.ui.fragments.profile.ProfileFragment;
+import com.northcoders.pigliotech_frontend.ui.fragments.signup.SignUpViewModel;
 
 
 public class LoginFragment extends Fragment {
@@ -32,7 +37,8 @@ public class LoginFragment extends Fragment {
     private ProgressBar progressBar;
     private FragmentLoginBinding binding;
     private FirebaseAuth mAuth;
-    private final ProfileFragment profileFragment = new ProfileFragment();
+    //    private final ProfileFragment profileFragment = new ProfileFragment();
+    private LoginViewModel viewModel;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -58,6 +64,9 @@ public class LoginFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // Create ViewModel Instance
+        viewModel = new ViewModelProvider(requireActivity()).get(LoginViewModel.class);
+
         editTextEmail = binding.email;
         editTextPassword = binding.password;
         progressBar = binding.progressBar;
@@ -67,13 +76,58 @@ public class LoginFragment extends Fragment {
 
         NavigationBarView bottomNav = getActivity().findViewById(R.id.bottom_nav_bar);
         bottomNav.setVisibility(View.GONE);
+
+        viewModel.getState().observe(requireActivity(), loginState -> {
+            if(loginState.getLoading()){
+                progressBar.setVisibility(View.VISIBLE);
+            }else {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
+        Context context = getContext();
+        FragmentActivity activity = getActivity();
+
+        /*
+        events observer that uses the state of MutableLiveData<Login> events in the
+        SignUpFragment ViewModel
+         */
+        viewModel.getEvents().observe(requireActivity(), loginEvent -> {
+            if(loginEvent != null){
+                switch(loginEvent){
+                    case LOGIN_SUCCESSFUL:
+                        // On successful login, create a toast and navigate to home fragment
+                        Toast.makeText(
+                                context,
+                                "Login successful!!",
+                                Toast.LENGTH_LONG
+                        ).show();
+
+                        activity.getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(
+                                        R.id.frame_layout_fragment,
+                                        new HomeFragment()
+                                ).commit();
+
+                        break;
+                    case LOGIN_FAILED:
+                        Toast.makeText(
+                                context,
+                                "Login failed!!",
+                                Toast.LENGTH_LONG
+                        ).show();
+                        break;
+                }
+                viewModel.eventSeen(); // Set the loginEvent back to null
+            }
+        });
     }
 
-    private void loginUserAccount()
-    {
+    private void loginUserAccount(){
 
         // show the visibility of progress bar to show loading
-        progressBar.setVisibility(View.VISIBLE);
+//        progressBar.setVisibility(View.VISIBLE);
 
         // Take the value of two edit texts in Strings
         String email, password;
@@ -97,49 +151,51 @@ public class LoginFragment extends Fragment {
             return;
         }
 
+        viewModel.login(email, password);
+
         // signin existing user
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(
-                        new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(
-                                    @NonNull Task<AuthResult> task)
-                            {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(getContext(),
-                                                    "Login successful!!",
-                                                    Toast.LENGTH_LONG)
-                                            .show();
-
-                                    // hide the progress bar
-                                    progressBar.setVisibility(View.GONE);
-
-                                    // if sign-in is successful
-                                    // intent to home activity
-                                    if (getActivity() != null){
-                                        getActivity().getSupportFragmentManager()
-                                                .beginTransaction()
-                                                .replace(
-                                                        R.id.frame_layout_fragment,
-                                                        profileFragment
-                                                ).commit();
-                                    }
-                                }
-
-                                else {
-
-                                    // sign-in failed
-                                    Toast.makeText(getContext(),
-                                                    "Login failed!!",
-                                                    Toast.LENGTH_LONG)
-                                            .show();
-
-                                    // hide the progress bar
-                                    progressBar.setVisibility(View.GONE);
-                                }
-                            }
-                        }
-                );
+//        mAuth.signInWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(
+//                        new OnCompleteListener<AuthResult>() {
+//                            @Override
+//                            public void onComplete(
+//                                    @NonNull Task<AuthResult> task)
+//                            {
+//                                if (task.isSuccessful()) {
+//                                    Toast.makeText(getContext(),
+//                                                    "Login successful!!",
+//                                                    Toast.LENGTH_LONG)
+//                                            .show();
+//
+//                                    // hide the progress bar
+//                                    progressBar.setVisibility(View.GONE);
+//
+//                                    // if sign-in is successful
+//                                    // intent to home activity
+//                                    if (getActivity() != null){
+//                                        getActivity().getSupportFragmentManager()
+//                                                .beginTransaction()
+//                                                .replace(
+//                                                        R.id.frame_layout_fragment,
+//                                                        profileFragment
+//                                                ).commit();
+//                                    }
+//                                }
+//
+//                                else {
+//
+//                                    // sign-in failed
+//                                    Toast.makeText(getContext(),
+//                                                    "Login failed!!",
+//                                                    Toast.LENGTH_LONG)
+//                                            .show();
+//
+//                                    // hide the progress bar
+//                                    progressBar.setVisibility(View.GONE);
+//                                }
+//                            }
+//                        }
+//                );
     }
 
 }
--- app/src/main/java/com/northcoders/pigliotech_frontend/ui/fragments/login/LoginViewModel.java
+++ app/src/main/java/com/northcoders/pigliotech_frontend/ui/fragments/login/LoginViewModel.java
@@ -1,6 +1,89 @@
 package com.northcoders.pigliotech_frontend.ui.fragments.login;
 
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.northcoders.pigliotech_frontend.model.service.AuthRepository;
+
 public class LoginViewModel extends ViewModel {
+
+    private final AuthRepository authRepository;
+
+    private final MutableLiveData<LoginState> state = new MutableLiveData<>(new LoginState(false));
+    private final MutableLiveData<LoginEvents> events = new MutableLiveData<>(null);
+
+    public LoginViewModel() {
+        this.authRepository = new AuthRepository();
+    }
+
+    public MutableLiveData<LoginState> getState() {
+        return state;
+    }
+
+    public MutableLiveData<LoginEvents> getEvents() {
+        return events;
+    }
+
+    public void eventSeen(){
+        events.setValue(null);
+    }
+
+    public void login(String email, String password){
+
+        // Update the state of the progressbar
+        state.setValue(new LoginState(true));
+
+        // signin existing user
+        authRepository.getmAuth()
+                .signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+//                                Toast.makeText(getContext(),
+//                                                "Login successful!!",
+//                                                Toast.LENGTH_LONG)
+//                                        .show();
+                                events.setValue(LoginEvents.LOGIN_SUCCESSFUL);
+
+                                // hide the progress bar
+//                                progressBar.setVisibility(View.GONE);
+                                state.setValue(new LoginState(false));
+
+                                // if sign-in is successful
+                                // intent to home activity
+//                                if (getActivity() != null){
+//                                    getActivity().getSupportFragmentManager()
+//                                            .beginTransaction()
+//                                            .replace(
+//                                                    R.id.frame_layout_fragment,
+//                                                    profileFragment
+//                                            ).commit();
+//                                }
+                            }
+
+                            else {
+
+                                // sign-in failed
+//                                Toast.makeText(getContext(),
+//                                                "Login failed!!",
+//                                                Toast.LENGTH_LONG)
+//                                        .show();
+//
+//                                // hide the progress bar
+//                                progressBar.setVisibility(View.GONE);
+
+                                events.setValue(LoginEvents.LOGIN_FAILED);
+
+                                // Update the state for the progressbar
+                                state.setValue(new LoginState(false));
+                            }
+                        }
+                );
+    }
+
 }
