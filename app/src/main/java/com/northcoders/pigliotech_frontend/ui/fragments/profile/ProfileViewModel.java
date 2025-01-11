fa1ee1603aa865e23ad3196239a86b6b690669e4
Tawanda Chabva <tchabva@gmail.com>
2025-01-11 16:58:46 +0000

    refactor: removed commented out codes and removed unneeded whitespaces

--- app/src/main/java/com/northcoders/pigliotech_frontend/ui/fragments/profile/ProfileViewModel.java
+++ app/src/main/java/com/northcoders/pigliotech_frontend/ui/fragments/profile/ProfileViewModel.java
@@ -6,7 +6,6 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.northcoders.pigliotech_frontend.model.User;
 import com.northcoders.pigliotech_frontend.model.service.AuthRepository;
 import com.northcoders.pigliotech_frontend.model.service.UserRepository;
@@ -19,14 +18,11 @@ public class ProfileViewModel extends ViewModel {
     private final AuthRepository authRepository;
     private final UserRepository userRepository;
 
-    private FirebaseUser currentUser;
     private final MutableLiveData<ProfileState> state = new MutableLiveData<>(
             new ProfileState.Loading()
     );
 
-    Consumer<User> userConsumer = user ->{
-
-
+    private final Consumer<User> userConsumer = user ->{
         if (user != null){
 
             Log.i("User Consumer Called", user.toString());
@@ -38,54 +34,18 @@ public class ProfileViewModel extends ViewModel {
                     new ArrayList<>()
             ));
         }
-
-
     };
-
-
 
     public ProfileViewModel() {
         this.authRepository = new AuthRepository();
         this.userRepository = new UserRepository();
-
-
     }
 
     public void load(){
-
         state.setValue(new ProfileState.Loading());
         String userID = authRepository.getmAuth().getCurrentUser().getUid();
         userRepository.getUser(userID, userConsumer);
     }
-
-
-
-    //    public LiveData<User> getUser(){
-//        // TODO: Null and progress bar state
-//
-//        this.currentUser = authRepository.getmAuth().getCurrentUser();
-//        if (currentUser != null){
-//            state.setValue(new ProfileSate(false));
-//            String uid = this.currentUser.getUid();
-//            Log.i("PROFILE-VM", uid);
-//            return userRepository.getUser(uid);
-//        }else{
-//            state.setValue(new ProfileSate(true));
-//            Log.i("PROFILE-VM", "2");
-//            return userRepository.getUser("2");
-//        }
-//
-//
-//
-//        /*
-//        if currentUser is null -> state or user loading user
-//            progress is loading
-//
-//         currentUser is not-null -> we update the state and then view should
-//
-//         */
-//
-//    }
 
     public MutableLiveData<ProfileState> getState() {
         return state;
