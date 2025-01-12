64d1bca73d0aaa34fca1f5170124bdd1d65ba0eb
Tawanda Chabva <tchabva@gmail.com>
2025-01-12 15:15:15 +0000

    feat: implement binding to AddBookFragment

--- app/src/main/java/com/northcoders/pigliotech_frontend/ui/fragments/addbook/AddBookFragment.java
+++ app/src/main/java/com/northcoders/pigliotech_frontend/ui/fragments/addbook/AddBookFragment.java
@@ -2,32 +2,55 @@ package com.northcoders.pigliotech_frontend.ui.fragments.addbook;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.northcoders.pigliotech_frontend.R;
-
+import com.northcoders.pigliotech_frontend.databinding.FragmentAddBookBinding;
 
 public class AddBookFragment extends Fragment {
 
+    private AddBookViewModel viewModel;
+    private EditText editTextIsbn;
+    private ProgressBar progressBar;
+    private Button buttonSubmit;
+    private FragmentAddBookBinding binding;
 
     public AddBookFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        viewModel = new ViewModelProvider(requireActivity()).get(AddBookViewModel.class);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_book, container, false);
+        binding = FragmentAddBookBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        editTextIsbn = binding.isbn;
+        progressBar = binding.progressBar;
+        buttonSubmit = binding.buttonSubmit;
     }
 }
