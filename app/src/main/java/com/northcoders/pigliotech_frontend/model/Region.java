e7b08eb5e1ff663a503771e18a2bb0d9e5a1ef8d
Tawanda Chabva <tchabva@gmail.com>
2025-01-13 20:25:59 +0000

    feat: add string value attributes to Regions enum class

--- app/src/main/java/com/northcoders/pigliotech_frontend/model/Region.java
+++ app/src/main/java/com/northcoders/pigliotech_frontend/model/Region.java
@@ -1,13 +1,21 @@
 package com.northcoders.pigliotech_frontend.model;
 
+import com.northcoders.pigliotech_frontend.R;
+
 public enum Region {
-    NORTH_WEST,
-    NORTH_EAST,
-    YORKSHIRE_HUMBER,
-    WEST_MIDLANDS,
-    EAST_MIDLANDS,
-    EAST_ANGLIA,
-    LONDON,
-    SOUTH_EAST,
-    SOUTH_WEST
+    NORTH_WEST(R.string.north_west),
+    NORTH_EAST(R.string.north_east),
+    YORKSHIRE_HUMBER(R.string.yorkshire_humber),
+    WEST_MIDLANDS(R.string.west_midlands),
+    EAST_MIDLANDS(R.string.east_midlands),
+    EAST_ANGLIA(R.string.east_anglia),
+    LONDON(R.string.london),
+    SOUTH_EAST(R.string.south_east),
+    SOUTH_WEST(R.string.south_west);
+
+    public final int region;
+
+    private Region(int region) {
+        this.region = region;
+    }
 }
