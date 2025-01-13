33933ac8ba0d997a2e10f13159ff0487ad41aa9b
Tawanda Chabva <tchabva@gmail.com>
2025-01-13 15:46:26 +0000

    feat: add Regions enum list

---
+++ app/src/main/java/com/northcoders/pigliotech_frontend/model/Region.java
@@ -0,0 +1,13 @@
+package com.northcoders.pigliotech_frontend.model;
+
+public enum Region {
+    NORTH_WEST,
+    NORTH_EAST,
+    YORKSHIRE_HUMBER,
+    WEST_MIDLANDS,
+    EAST_MIDLANDS,
+    EAST_ANGLIA,
+    LONDON,
+    SOUTH_EAST,
+    SOUTH_WEST
+}
