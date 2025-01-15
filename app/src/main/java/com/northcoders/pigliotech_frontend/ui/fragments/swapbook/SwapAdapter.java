8027614f343a22e84164de793431b61857dff631
Joel Cohen <jcohenf50@gmail.com>
2025-01-15 14:20:17 +0000

    feat: created Swap adapter and viewmodel classes

---
+++ app/src/main/java/com/northcoders/pigliotech_frontend/ui/fragments/swapbook/SwapAdapter.java
@@ -0,0 +1,6 @@
+package com.northcoders.pigliotech_frontend.ui.fragments.swapbook;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SwapAdapter {
+}
---
+++ app/src/main/java/com/northcoders/pigliotech_frontend/ui/fragments/swapbook/SwapViewModel.java
@@ -0,0 +1,9 @@
+package com.northcoders.pigliotech_frontend.ui.fragments.swapbook;
+
+import androidx.lifecycle.ViewModel;
+
+public class SwapViewModel extends ViewModel {
+
+
+
+}
