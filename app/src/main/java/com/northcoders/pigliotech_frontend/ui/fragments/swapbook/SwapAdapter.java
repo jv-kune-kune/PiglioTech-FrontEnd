5f93d28469eb006e278ba6fcffd551a89f5aa2bc
Tawanda Chabva <tchabva@gmail.com>
2025-01-15 22:15:46 +0000

    feat: implement logic for SwapAdapter

--- app/src/main/java/com/northcoders/pigliotech_frontend/ui/fragments/swapbook/SwapAdapter.java
+++ app/src/main/java/com/northcoders/pigliotech_frontend/ui/fragments/swapbook/SwapAdapter.java
@@ -1,6 +1,56 @@
 package com.northcoders.pigliotech_frontend.ui.fragments.swapbook;
 
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class SwapAdapter {
+import com.northcoders.pigliotech_frontend.databinding.SwapItemBinding;
+
+import java.util.List;
+
+public class SwapAdapter extends RecyclerView.Adapter<SwapAdapter.SwapViewHolder> {
+
+    // TODO: To implement
+    private final List<Object> swaps;
+
+    public SwapAdapter(List<Object> swaps) {
+        this.swaps = swaps;
+    }
+
+    @NonNull
+    @Override
+    public SwapViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        SwapItemBinding binding = SwapItemBinding.inflate(
+                LayoutInflater.from(parent.getContext()),
+                parent,
+                false
+        );
+
+        return new SwapViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SwapViewHolder holder, int position) {
+
+        Object swap = swaps.get(position); // TODO
+        // TODO implement data binding in the xml
+        // holder.swapItemBinding.setSwap(swap)
+    }
+
+    @Override
+    public int getItemCount() {
+        return swaps.size();
+    }
+
+    public static class SwapViewHolder extends RecyclerView.ViewHolder{
+        private final SwapItemBinding swapItemBinding;
+
+        public SwapViewHolder(SwapItemBinding swapItemBinding) {
+            super(swapItemBinding.getRoot());
+            this.swapItemBinding = swapItemBinding;
+        }
+    }
 }
