d3d47c83887b62af113f5657d7591c6896ac5dda
Tawanda Chabva <tchabva@gmail.com>
2025-01-15 14:41:32 +0000

    feat: add NETWORK_ERROR to Events Enum

--- app/src/main/java/com/northcoders/pigliotech_frontend/ui/fragments/addbook/AddBookEvents.java
+++ app/src/main/java/com/northcoders/pigliotech_frontend/ui/fragments/addbook/AddBookEvents.java
@@ -3,5 +3,6 @@ package com.northcoders.pigliotech_frontend.ui.fragments.addbook;
 public enum AddBookEvents {
     BOOK_ADDED,
     BOOK_NOT_ADDED,
-    INVALID_ISBN
+    INVALID_ISBN,
+    NETWORK_ERROR
 }
