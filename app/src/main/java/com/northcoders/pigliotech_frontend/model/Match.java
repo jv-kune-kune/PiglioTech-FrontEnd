bba0036f00871a6bd70714eb5fc0c8d75ae00f13
Tawanda Chabva <tchabva@gmail.com>
2025-01-17 12:35:02 +0000

    fix: updated variable names to match the JSON object

--- app/src/main/java/com/northcoders/pigliotech_frontend/model/Match.java
+++ app/src/main/java/com/northcoders/pigliotech_frontend/model/Match.java
@@ -1,4 +1,4 @@
 package com.northcoders.pigliotech_frontend.model;
 
-public record Match(Long id, User userOne, User userTwo, Book userBookOne, Book userBookTwo) {
+public record Match(Long id, User userOne, User userTwo, Book userOneBook, Book userTwoBook) {
 }
